8f97df8222b3bdeae1db7e1b11b03ae3b98734f3
Pookengkay <minadanielajenrose_bsit@plmun.edu.ph>
2025-02-27 09:25:14 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -1,15 +1,15 @@
-def get_student_score():
+def get_student_score() -> float:
     while True:
         try:
-            score = float(input("Enter your score: "))  
+            score = float(input("Enter your score: "))
             if 0 <= score <= 100:
-                return score  
+                return score
             else:
-                print("Please enter a valid score between 0 and 100.")
+                print("Please enter a score between 0 and 100.")
         except ValueError:
             print("Invalid input. Please enter a numerical value.")
 
-def calculate_grade(score):
+def calculate_grade(score: float) -> str:
     if score >= 90:
         return 'A'
     elif score >= 80:
@@ -21,10 +21,7 @@ def calculate_grade(score):
     else:
         return 'F'
 
-def main():
-    score = get_student_score()
-    grade = calculate_grade(score)
-    print(f"Your Grade is: {grade}")
-
 if __name__ == "__main__":
-    main()
+    student_score = get_student_score()
+    grade = calculate_grade(student_score)
+    print(f"Your Grade is: {grade}")
